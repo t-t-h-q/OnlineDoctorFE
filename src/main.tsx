6d9b5991dfd9415fc79a7ad17d50c59b7e38484a
vvquangit <vuquangit@gmail.com>
2024-10-12 22:35:37 +0700

    fix: remove some code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import App from './App.tsx'
-import './index.css'
 import '@/styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -10,5 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <ConfigProvider>
       <App />
     </ConfigProvider>
-  </StrictMode>
+  </StrictMode>,
 )
