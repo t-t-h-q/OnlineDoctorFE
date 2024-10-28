69621baa284f15bce539be8e1bc19abdf5152fb2
vvquangit <vuquangit@gmail.com>
2024-10-28 10:55:17 +0700

    feat: redirect if user is alreaday logged in

--- src/pages/auth/Login/hooks/useLogin.ts
+++ src/pages/auth/Login/hooks/useLogin.ts
@@ -39,7 +39,7 @@ const useLogin = () => {
       const pathToRedirect = getRolePath(currentUser.role.name)
       navigate(pathToRedirect)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUser])
 
   return {
